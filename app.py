cf4516953a7d35e066379b0ea08af66d020bf0d9
lde2015 <laurence.dewaele@nordnet.fr>
2022-06-12 17:22:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -105,10 +105,10 @@ image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
 
 if image_file is not None:
 
-	# To See details
-	#file_details = {"filename":image_file.name, "filetype":image_file.type,
-    #                "filesize":image_file.size}
-	#st.write(file_details)
+    # To See details
+    file_details = {"filename":image_file.name, "filetype":image_file.type,
+                   "filesize":image_file.size}
+    st.write(file_details)
 
     # To View Uploaded Image
     img = load_image(image_file)
